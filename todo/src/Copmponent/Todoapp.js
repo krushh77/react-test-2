9ae1cf163ab2512b858c83e59a25b19b2e2c37e4
krushh77 <145192360+krushh77@users.noreply.github.com>
2024-01-13 16:01:53 +0530

    testtodo

---
+++ todo/src/Copmponent/Todoapp.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import { useState } from 'react'
+
+
+const Todoapp = () => {
+
+const [activity, setactivity] = useState("");
+const [todolist, settodolist] = useState([]);
+
+function addingtodo (){
+    settodolist((todolist)=>{
+        const updated = [...todolist,activity];
+
+        setactivity=("")
+
+        return updated
+    })
+}
+
+function todochange (i){
+    const updateddata = todolist.filter((Eleme,id)=>{
+        return i!=id;
+    })
+
+    settodolist(updateddata);
+
+
+}
+
+
+  return (
+    <>
+    <h1>Todo list</h1>
+    <input typeof='text' placeholder='todo list' value={activity}  onChange={(e)=>setactivity(e.target.value)}/>
+    <button onClick={addingtodo}>Add</button>
+    <h1>todolist</h1>
+    {todolist ![] && }
+    <p key={i}>
+        <div>{data}</div>
+        <button onClick={todochange}>delete</button>
+
+    </p>
+
+
+
+
+    
+    </>
+  )
+}
+
+export default Todoapp
